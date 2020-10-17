8194e95a0a6b1d8101085e74b0150e043934630f
IDLKZ <idlkz2019@gmail.com>
2020-10-17 16:03:19 +0600

    Update first

---
+++ database/migrations/2020_10_17_094456_create_courses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            //Foreign key
+            $table->foreignId("author_id");
+            $table->foreign("author_id")->references("id")->on("users")->cascadeOnDelete();
+            $table->foreignId("language_id");
+            $table->foreign("language_id")->references("id")->on("languages")->cascadeOnDelete();
+            //
+            $table->string("img")->nullable();
+            $table->string("title");
+            $table->string("subtitle",500);
+            $table->text("description");
+            $table->json("advantage")->nullable();
+            $table->json("requirement")->nullable();
+            $table->boolean("status")->default(false);
+            $table->string("alias");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
