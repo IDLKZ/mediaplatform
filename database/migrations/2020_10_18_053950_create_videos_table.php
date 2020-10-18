7cb9d41c9477f4f7c4e0fe93a58281f63b8db9ef
IDLKZ <idlkz2019@gmail.com>
2020-10-18 13:02:21 +0600

    Update first

---
+++ database/migrations/2020_10_18_053950_create_videos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->foreignId("course_id");
+            $table->foreign("course_id")->references("id")->on("courses")->cascadeOnDelete();
+            $table->integer("available")->default(0);
+            $table->integer("count");
+            $table->string("title");
+            $table->text('description')->nullable();
+            $table->string("alias");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
