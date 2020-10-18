1a0b0851ebcc920de0a9866537f60f564c9315c3
idlkz <idlkz2019@gmail.com>
2020-10-18 23:18:55 +0600

    second commit

---
+++ database/migrations/2016_07_29_171118_create_chatter_discussion_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatterDiscussionTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('chatter_discussion', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('chatter_category_id')->unsigned()->default('1');
+            $table->string('title');
+            $table->bigInteger('user_id')->unsigned();
+            $table->boolean('sticky')->default(false);
+            $table->bigInteger('views')->unsigned()->default('0');
+            $table->boolean('answered')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('chatter_discussion');
+    }
+}
