30b74c00224f3d615d290d39cf8644b935513e74
IDLKZ <idlkz2019@gmail.com>
2020-10-18 23:19:51 +0600

    Update first

---
+++ database/migrations/2020_10_18_122759_create_materials_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->bigIncrements("id");
+
+            $table->foreignId("video_id");
+            $table->foreign("video_id")->references("id")->on("videos")->cascadeOnDelete();
+            $table->foreignId("author_id");
+            $table->foreign("author_id")->references("id")->on("users")->cascadeOnDelete();
+            $table->string("file")->nullable();
+            $table->string("title");
+            $table->string("type");
+            $table->json("links")->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+}
