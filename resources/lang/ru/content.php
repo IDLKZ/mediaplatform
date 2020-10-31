458e7bfe76a133416c1e9bb843dcfed6600a9e79
idlkz <idlkz2019@gmail.com>
2020-10-31 23:27:46 +0600

    second commit

---
+++ resources/lang/ru/content.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    //subscribers
+    'request' => 'Заявки',
+    'listeners' => 'Слушатели',
+    'course_title' => 'Наименование курса',
+    'sentTime' => 'время отправки запроса',
+    'sentTime' => 'Одобрен',
+
+
+    //buttons
+    'save' => 'сохранить',
+    'back' => 'назад',
+    'clear' => 'очистить поля',
+    'delete' => 'удалить',
+    'cancel' => 'отменить',
+    'new_video' => 'новое видео',
+    'new_video' => 'новый материал',
+
+];
