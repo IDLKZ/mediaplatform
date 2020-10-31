56646804a99efe209df2c872d380ade92807b350
IDLKZ <idlkz2019@gmail.com>
2020-11-01 00:33:49 +0600

    Update first

--- resources/lang/ru/content.php
+++ resources/lang/ru/content.php
@@ -7,17 +7,62 @@ return [
     'course_title' => 'Наименование курса',
     'sendTime' => 'Время отправки запроса',
     'approved' => 'Одобрен',
-    'denied' => 'Одобрен',
+    'requested' => 'Не одобрен',
+    'accept'=>"Принять",
+    'deny'=>'Отказать',
+    'detail'=>"Детали",
+
+    //course
+    "course"=>"Курс",
+    'new_course'=>"Новый видеокурс",
+    'course_subtitle'=>"Подзаголовок курса",
+    "course_description"=>"Описание курса",
+    "course_advantages"=>"Преимущества курса",
+    "course_requirements"=>"Требования курса",
+    "course_language"=>"Язык курса",
+    "course_image"=>"Изображение курса",
+    "course_status"=>"Курс готов к выпуску",
+
+    //video
+    "video"=>"Видео",
+    'new_video'=>"Новое видео",
+    "video_count"=>"Номер видео",
+    "video_video"=>"Видео",
+    "video_preview"=>"Разрешенный предпросмотр",
+    "my_video"=>"Мои видеоуроки",
+    "video_allowed"=>"Превью",
+    "video_notallowed"=>"Не превью",
+
+    //material
+    "material"=>"Материал",
+    "filename"=>"Наименование файла",
+    "links"=>"Ссылки",
+    "material_type"=>"Тип материала",
+    "material_video"=>"Файлы к материалу",
+
+    //examination
+    "examination"=>"Экзамены",
+    "new_examination"=>"Новые экзамены",
+    "examination_type"=>"Тип экзамена",
+    "examination_base"=>"База вопросов",
+    "examination_description"=>"Описание к экзамену",
+    "examination_material"=>"Материалы к экзамену",
 
 
 
     //buttons
-    'save' => 'сохранить',
-    'back' => 'назад',
-    'clear' => 'очистить поля',
-    'delete' => 'удалить',
-    'cancel' => 'отменить',
-    'new_video' => 'новое видео',
-    'new_video' => 'новый материал',
+    'save' => 'Сохранить',
+    'change'=>"Изменить",
+    'back' => 'Назад',
+    'clear' => 'Очистить поля',
+    'delete' => 'Удалить',
+    'cancel' => 'Отменить',
+    "edit"=>"Редактировать",
+    "show"=>"Детали",
+    "watch"=>"Смотреть",
+    "action"=>"Действия",
+    "download"=>"Скачать"
+
+
 
 ];
