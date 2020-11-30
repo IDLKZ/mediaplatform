edb481627141ffb932863ab894e26033074beb63
idlkz <idlkz2019@gmail.com>
2020-11-30 15:05:18 +0600

    second commit

--- resources/views/student/footer.blade.php
+++ resources/views/student/footer.blade.php
@@ -4,31 +4,32 @@
             <div class="col-md-9">
                 <div class="margin-50"></div>
                 <ul class="row text-left">
-                    <div class="row desk-none footer-logos pl-2">
-                        <img src="/frontend/images/jascongress.png" alt="" width="75" height="100">
-                        <img src="/frontend/images/nazarbaev_foundation.png" alt="" width="95" height="100">
+                    <div class="row desk-none footer-logos">
+                        <a href="https://fnn.kz/ru/main/index"><img src="/frontend/images/nazarbaev_foundation.png" alt="" width="95" height="100"></a>
+                        <a href="https://jascongress.kz/"><img src="/frontend/images/jascongress.png" alt="" width="75" height="100"></a>
                     </div>
 
                     <li class="mob-none">
-                        <img src="/frontend/images/jascongress.png" alt="" width="75" height="100">
+                        <a href="https://fnn.kz/ru/main/index"><img src="/frontend/images/nazarbaev_foundation.png" alt="" width="95" height="100"></a>
                     </li>
+
                     <li class="mob-none">
-                        <img src="/frontend/images/nazarbaev_foundation.png" alt="" width="95" height="100">
+                        <a href="https://jascongress.kz/"><img src="/frontend/images/jascongress.png" alt="" width="75" height="100"></a>
                     </li>
                     <li class="text-center">
-                        <a href="{{route('frontCourses')}}">{{__('front.courses')}}</a>
+                        <a href="{{route('student.course')}}">{{__('front.courses')}}</a>
                     </li>
                     <li class="">
-                        <a href="{{route('frontExam')}}">{{__('front.exam')}}</a>
+                        <a href="{{route('student.checkedResult')}}">{{__('front.exam')}}</a>
                     </li>
                     {{--                    <li class="text-center">--}}
                     {{--                        <a href="{{route('frontDiscussion')}}">Отзывы</a>--}}
                     {{--                    </li>--}}
                     <li class="text-center">
-                        <a href="{{route('frontDiscussion')}}">{{__('front.discuss')}}</a>
+                        <a href="{{route('forums')}}">{{__('front.discuss')}}</a>
                     </li>
                     <li class="text-center">
-                        <a href="{{route('frontCertificate')}}">{{__('front.certificate')}}</a>
+                        <a href="{{route('myCertificates')}}">{{__('front.certificate')}}</a>
                     </li>
                     <div class="row desk-none footer-logos mt-2 mr-3">
                         <a href="https://www.youtube.com/channel/UCh4-ssE18BF8J9MpjRJO-9A" class="social-icon">
@@ -67,7 +68,7 @@
             <div class="col-md-10 col-sm-12">
                 <div class="row justify-content-end">
                     <a href="#" class="footer-info">{{__('front.contract')}}</a>
-                    <a href="#" class="footer-info">{{__('front.payment')}}</a>
+{{--                    <a href="#" class="footer-info">{{__('front.payment')}}</a>--}}
                     <a href="#" class="footer-info">{{__('front.privacy_policy')}}</a>
                 </div>
             </div>
